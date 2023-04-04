ba00e4c9ac55ef739fe61a3073e4389b756208e7
Jason-Wall <jason.waldick@gmail.com>
2023-04-04 13:30:57 -0600

    Create dashboard and styling

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,13 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
   def show
+    @categories = Category
+    .select('categories.*, COUNT(products.*) as products_count')
+    .joins(:products)
+    .group('categories.id')
+    @category_count = Category.count
+    
+    @product_total = Product.count
+                          
   end
 end
