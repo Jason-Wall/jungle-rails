f8efbbe445a3963f67f50db634c61b2ba5bfd68a
Jason-Wall <jason.waldick@gmail.com>
2023-04-10 17:06:42 -0600

    Finish testing user model

--- spec/models/product_model_spec.rb
+++ spec/models/product_model_spec.rb
@@ -1,80 +1,76 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe '.Product' do
     it 'Create saves a product to the db' do
       category = Category.create(
-        name:'houseplant'
+        name: 'houseplant'
       )
       product = Product.create(
         name: 'Plant',
         price: 100,
         quantity: 2,
-        category: category
+        category:
       )
       expect(Product.count).to eq(1)
     end
 
-
     it 'Errors if name is blank' do
       category = Category.create(
-        name:'houseplant'
+        name: 'houseplant'
       )
       product = Product.new(
         price: 100,
         quantity: 2,
-        category: category
+        category:
       )
       product.save
       expect(product.errors.full_messages).to include("Name can't be blank")
       expect(User.count).to eq(0)
     end
 
-
     it 'Errors if price is blank' do
       category = Category.create(
-        name:'houseplant'
+        name: 'houseplant'
       )
       product = Product.new(
         name: 'Plant',
         quantity: 2,
-        category: category
+        category:
       )
       product.save
       expect(product.errors.full_messages).to include("Price can't be blank")
       expect(User.count).to eq(0)
     end
 
-
     it 'Errors if quantity is blank' do
       category = Category.create(
-        name:'houseplant'
+        name: 'houseplant'
       )
       product = Product.new(
         price: 100,
         name: 'Plant',
-        category: category
+        category:
       )
       product.save
       expect(product.errors.full_messages).to include("Quantity can't be blank")
       expect(User.count).to eq(0)
     end
 
-
     it 'Errors if category is blank' do
       category = Category.create(
-        name:'houseplant'
+        name: 'houseplant'
       )
       product = Product.new(
         price: 100,
         quantity: 2,
-        name: 'Plant',
+        name: 'Plant'
       )
       product.save
       expect(product.errors.full_messages).to include("Category can't be blank")
       expect(User.count).to eq(0)
     end
-
-
   end
 end
--- spec/models/user_model_spec.rb
+++ spec/models/user_model_spec.rb
@@ -1,87 +1,111 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe '.User' do
-      it 'Create saves a user to the db' do
-        user = User.create(
-          first_name: 'John',
-          last_name: 'Doe',
-          email: 'test@test.com',
-          password: 'tests',
-          password_confirmation: 'tests'
-        )
-        expect(User.count).to eq(1)
-      end
-
-
-      it 'Error if passwords do not match a user to the db' do
-        user = User.create(
-          first_name: 'John',
-          last_name: 'Doe',
-          email: 'test@test.com',
-          password: 'tests',
-          password_confirmation: 'not the right password'
-        )
-        expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
-        expect(User.count).to eq(0)
-      end
-
-      it 'Error if an email already exists in the db, not case sensitive' do
-        user = User.create(
-          first_name: 'John',
-          last_name: 'Doe',
-          email: 'test@TEST.com',
-          password: 'tests',
-          password_confirmation: 'tests'
-        )
-        newuser = User.create(
-          first_name: 'John',
-          last_name: 'Doe',
-          email: 'test@test.com',
-          password: 'tests',
-          password_confirmation: 'tests'
-        )
-        expect(newuser.errors.full_messages).to include("Email has already been taken")
-        expect(User.count).to eq(1)
-      end
-
-      it 'Error if password is less than 4 characters' do
-        user = User.create(
-          first_name: 'John',
-          last_name: 'Doe',
-          email: 'test@test.com',
-          password: 'tes',
-          password_confirmation: 'tes'
-        )
-        expect(user.errors.full_messages).to include("Password is too short (minimum is 4 characters)")
-        expect(User.count).to eq(0)
-      end
-    end
-
-    describe '#authenticate_with_credentials'do
-      it 'returns a user if password is correct'do
-        user = User.create(
+    it 'Create saves a user to the db' do
+      user = User.create(
         first_name: 'John',
         last_name: 'Doe',
         email: 'test@test.com',
         password: 'tests',
         password_confirmation: 'tests'
-        )
-        user_test = User.authenticate_with_credentials('test@test.com', 'tests')
-        expect(user_test).to be_a(User)
-      end
+      )
+      expect(User.count).to eq(1)
+    end
 
+    it 'Error if passwords do not match a user to the db' do
+      user = User.create(
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'test@test.com',
+        password: 'tests',
+        password_confirmation: 'not the right password'
+      )
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      expect(User.count).to eq(0)
+    end
 
-      it 'returns nil if password is incorrect'do
-        user = User.create(
-          first_name: 'John',
-          last_name: 'Doe',
-          email: 'test@test.com',
-          password: 'tests',
-          password_confirmation: 'tests'
-        )
-        user_test = User.authenticate_with_credentials('test@test.com', 'bad password')
-        expect(user_test).to be(nil)
-      end  
+    it 'Error if an email already exists in the db, not case sensitive' do
+      user = User.create(
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'test@TEST.com',
+        password: 'tests',
+        password_confirmation: 'tests'
+      )
+      newuser = User.create(
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'test@test.com',
+        password: 'tests',
+        password_confirmation: 'tests'
+      )
+      expect(newuser.errors.full_messages).to include('Email has already been taken')
+      expect(User.count).to eq(1)
+    end
+
+    it 'Error if password is less than 4 characters' do
+      user = User.create(
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'test@test.com',
+        password: 'tes',
+        password_confirmation: 'tes'
+      )
+      expect(user.errors.full_messages).to include('Password is too short (minimum is 4 characters)')
+      expect(User.count).to eq(0)
     end
   end
+
+  describe '#authenticate_with_credentials' do
+    it 'returns a user if password is correct' do
+      user = User.create(
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'test@test.com',
+        password: 'tests',
+        password_confirmation: 'tests'
+      )
+      user_test = User.authenticate_with_credentials('test@test.com', 'tests')
+      expect(user_test).to be_a(User)
+    end
+
+    it 'returns nil if password is incorrect' do
+      user = User.create(
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'test@test.com',
+        password: 'tests',
+        password_confirmation: 'tests'
+      )
+      user_test = User.authenticate_with_credentials('test@test.com', 'bad password')
+      expect(user_test).to be(nil)
+    end
+
+    it 'returns a user even if email case is not correct' do
+      user = User.create(
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'test@test.com',
+        password: 'tests',
+        password_confirmation: 'tests'
+      )
+      user_test = User.authenticate_with_credentials('TEST@test.com', 'tests')
+      expect(user_test).to be_a(User)
+    end
+
+    it 'returns a user even if email has whitespace' do
+      user = User.create(
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'test@test.com',
+        password: 'tests',
+        password_confirmation: 'tests'
+      )
+      user_test = User.authenticate_with_credentials('  TEST@test.com ', 'tests')
+      expect(user_test).to be_a(User)
+    end
+  end
+end
