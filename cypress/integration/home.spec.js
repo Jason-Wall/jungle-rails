7eaa6634009915ae28e9495e2b2ffbdd67f4051c
Jason-Wall <jason.waldick@gmail.com>
2023-04-10 18:20:20 -0600

    Product detail page working in cypress

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,14 +3,18 @@ describe('Home page', () => {
     cy.visit('localhost:3000');
   });
 
-  it('loads the title banner', () => {
+  it('Loads the title banner', () => {
     cy.get('.title')
       .find('h1')
       .should('have.text', 'The Jungle');
   });
 
-  it("There is products on the page", () => {
+  it("There are products on the page", () => {
     cy.get(".products article").should("be.visible");
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
   });
 
 }
---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,15 @@
+describe('Home page', () => {
+  beforeEach(() => {
+    cy.visit('localhost:3000');
+    cy.get(".products article")
+      .first()
+      .click();
+  });
+
+
+  it('Loads the selected item', () => {
+    cy.get(".product-detail").should("be.visible");
+  });
+
+}
+);
